1fcc412d56d3414b74337f376f523514b8723e62
osiyuk <kiril.saradomin@gmail.com>
2018-03-16 21:27:35 +0300

    lexing translating char stream to token stream

--- ion.c
+++ ion.c
@@ -1,7 +1,9 @@
 #include "stretchy_buffer.h"
+#include "lexing.h"
 
 int main(int argc, char **argv)
 {
         buf_test();
+        lex_test();
         return 0;
 }
---
+++ lexing.h
@@ -0,0 +1,80 @@
+#ifndef ION_LEXING
+#define ION_LEXING
+
+#include <stdint.h>
+#include <ctype.h>
+#include <stdio.h>
+
+typedef enum {
+        TOKEN_INT = 128,
+        TOKEN_NAME
+} kind_t;
+
+struct Token {
+        kind_t kind;
+        union {
+                uint64_t val;
+                struct {
+                        const char *start;
+                        size_t length;
+                };
+        };
+};
+
+struct Token token;
+const char *stream;
+
+
+void next_token()
+{
+        switch (*stream) {
+        case '0'...'9':
+                token.kind = TOKEN_INT;
+                token.val = 0;
+                while (isdigit(*stream)) {
+                        token.val *= 10;
+                        token.val += (*stream - '0');
+                        stream++;
+                }
+                break;
+        case 'a'...'z':
+        case 'A'...'Z':
+        case '_':
+                token.kind = TOKEN_NAME;
+                token.start = stream++;
+                while (isalnum(*stream)
+                        || *stream == '_') stream++;
+                token.length = stream - token.start;
+                break;
+        default:
+                token.kind = *stream++;
+        }
+}
+
+
+void print_token()
+{
+        switch (token.kind) {
+        case TOKEN_INT:
+                printf("TOKEN_INT = %lu\n", token.val);
+                break;
+        case TOKEN_NAME:
+                printf("TOKEN_NAME : %.*s\n", token.length, token.start);
+                break;
+        default:
+                printf(isprint(token.kind) ?
+                        "TOKEN '%c'\n" :
+                        "TOKEN %d\n", token.kind);
+        }
+}
+
+void lex_test()
+{
+        stream = "var max_exprs = 1024\n\nconst PI = 3.14";
+        while (next_token(), token.kind) {
+                print_token();
+        }
+}
+
+#endif
+
