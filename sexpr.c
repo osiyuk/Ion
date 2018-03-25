6745667063f4db5e2412abe6f5bb95b09e354cec
osiyuk <kiril.saradomin@gmail.com>
2018-03-25 21:57:26 +0300

    parser more compact output format

--- sexpr.c
+++ sexpr.c
@@ -58,6 +58,8 @@ void next()
         case '-': token = OP_SUB; break;
         case '|': token = OP_OR; break;
         case '^': token = OP_XOR; break;
+        
+        case 0: return;
         default: fatal("expected VALID token, got '%c'", *stream);
         }
         stream++;
@@ -72,12 +74,10 @@ void next()
 
 void parse_unary()
 {
-        puts("parse_unary()");
-        
         if (token == OP_SUB || token == OP_TILDE) {
                 switch (token) {
-                case OP_SUB: puts("PARSED OP_SUB"); break;
-                case OP_TILDE: puts("PARSED OP_TILDE"); break;
+                case OP_SUB: puts("OP_SUB"); break;
+                case OP_TILDE: puts("OP_TILDE"); break;
                 }
                 next();
         }
@@ -85,25 +85,24 @@ void parse_unary()
         if (token != INT)
                 fatal("expected INT token, got '%c'", *stream);
         
-        printf("PARSED INT %d\n", val);
+        printf("INT %d\n", val);
         next();
+        return;
 }
 
 
 void parse_factor()
 {
-        puts("parse_factor()");
-        
         parse_unary();
         
-        if (OP_MUL <= token && token <= OP_AND) {
+        while (OP_MUL <= token && token <= OP_AND) {
                 switch (token) {
-                case OP_MUL: puts("PARSED OP_MUL"); break;
-                case OP_DIV: puts("PARSED OP_DIV"); break;
-                case OP_MOD: puts("PARSED OP_MOD"); break;
-                case OP_LSHIFT: puts("PARSED OP_LSHIFT"); break;
-                case OP_RSHIFT: puts("PARSED OP_RSHIFT"); break;
-                case OP_AND: puts("PARSED OP_AND"); break;
+                case OP_MUL: puts("OP_MUL"); break;
+                case OP_DIV: puts("OP_DIV"); break;
+                case OP_MOD: puts("OP_MOD"); break;
+                case OP_LSHIFT: puts("OP_LSHIFT"); break;
+                case OP_RSHIFT: puts("OP_RSHIFT"); break;
+                case OP_AND: puts("OP_AND"); break;
                 }
                 next();
                 parse_unary();
@@ -113,16 +112,14 @@ void parse_factor()
 
 void parse_term()
 {
-        puts("parse_term()");
-        
         parse_factor();
         
-        if (OP_ADD <= token && token <= OP_XOR) {
+        while (OP_ADD <= token && token <= OP_XOR) {
                 switch (token) {
-                case OP_ADD: puts("PARSED OP_ADD"); break;
-                case OP_SUB: puts("PARSED OP_SUB"); break;
-                case OP_OR: puts("PARSED OP_OR"); break;
-                case OP_XOR: puts("PARSED OP_XOR"); break;
+                case OP_ADD: puts("OP_ADD"); break;
+                case OP_SUB: puts("OP_SUB"); break;
+                case OP_OR: puts("OP_OR"); break;
+                case OP_XOR: puts("OP_XOR"); break;
                 }
                 next();
                 parse_factor();
@@ -133,6 +130,7 @@ void parse_term()
 void parse_expr(const char *str)
 {
         stream = str;
+        printf("PARSING %s\n", stream);
         next();
         parse_term();
 }
