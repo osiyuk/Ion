57f850a57e4188103bdb87c99c146dc279b5ba48
osiyuk <kiril.saradomin@gmail.com>
2018-05-19 02:59:21 +0300

    print statements printer

--- ast_print.h
+++ ast_print.h
@@ -128,6 +128,120 @@ void print_type(Typespec *type)
         }
 }
 
+
+void print_stmt(Stmt *stmt)
+{
+        const Stmt s = *stmt;
+        
+        switch (stmt->kind) {
+        case STMT_BREAK:
+                printf("(break)");
+                return;
+        case STMT_CONTINUE:
+                printf("(continue)");
+                return;
+        case STMT_RETURN:
+                printf("(return");
+                if (stmt->expr) {
+                        printf(" ");
+                        print_expr(stmt->expr);
+                }
+                printf(")");
+                return;
+        case STMT_IF:
+                printf("(if ");
+                print_expr(s.if_stmt.cond);
+                printf(" ");
+                print_stmt(s.if_stmt.body);
+                if (s.if_stmt.other) {
+                        printf(" else ");
+                        print_stmt(s.if_stmt.other);
+                }
+                printf(")");
+                return;
+        case STMT_WHILE:
+                printf("(while ");
+                print_expr(s.while_stmt.cond);
+                printf(" ");
+                print_stmt(s.while_stmt.body);
+                printf(")");
+                return;
+        case STMT_DO_WHILE:
+                printf("(do_while ");
+                print_stmt(s.while_stmt.body);
+                printf(" ");
+                print_expr(s.while_stmt.cond);
+                printf(")");
+                return;
+        case STMT_FOR:
+                printf("(for (");
+                if (s.for_stmt.init) {
+                        StmtList *init = s.for_stmt.init;
+                        for (size_t i = 0; i < init->num_stmt; i++) {
+                                printf(", ");
+                                print_stmt(init->stmt[i]);
+                        }
+                }
+                printf(") (");
+                if (s.for_stmt.cond) {
+                        print_expr(s.for_stmt.cond);
+                }
+                printf(") (");
+                if (s.for_stmt.step) {
+                        StmtList *step = s.for_stmt.step;
+                        for (size_t i = 0; i < step->num_stmt; i++) {
+                                printf(", ");
+                                print_stmt(step->stmt[i]);
+                        }
+                }
+                printf(") ");
+                print_stmt(s.for_stmt.body);
+                printf(")");
+                return;
+        case STMT_SWITCH:
+                printf("(switch ");
+                print_expr(s.switch_stmt.expr);
+                printf(" ");
+                for (size_t i = 0; i < s.switch_stmt.num_cases; i++) {
+                        SwitchCase *sc = s.switch_stmt.cases[i];
+                        printf(" ");
+                        if (sc->expr) {
+                                printf("(case ");
+                                print_expr(sc->expr);
+                                printf(" ");
+                                print_stmt(sc->stmt);
+                                printf(")");
+                        } else {
+                                printf("(default ");
+                                print_stmt(sc->stmt);
+                                printf(")");
+                        }
+                }
+                printf(")");
+                return;
+        case STMT_BLOCK:
+                printf("(block");
+                for (size_t i = 0; i < s.block.num_stmt; i++) {
+                        printf(" ");
+                        print_stmt(s.block.stmt[i]);
+                }
+                printf(")");
+                return;
+        case STMT_EXPR:
+                print_expr(stmt->expr);
+                return;
+        case STMT_ASSIGN:
+                printf("(%s ", token_kind(s.assign.op));
+                print_expr(s.assign.lvalue);
+                printf(" ");
+                print_stmt(s.assign.rvalue);
+                printf(")");
+                return;
+        default:
+                assert(STMT_NONE);
+        }
+}
+
 #undef printf
 #endif
 
